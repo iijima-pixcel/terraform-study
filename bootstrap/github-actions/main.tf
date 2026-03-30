2be8c54c7f5ec090b93528f7f3935da63460976d
iijima-pixcel <144035356+iijima-pixcel@users.noreply.github.com>
2026-03-30 17:17:40 +0900

    cdパイプラインの追加

---
+++ bootstrap/github-actions/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Project     = var.project
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
+
+module "github_actions_role" {
+  source = "../../modules/iam/github_actions_role"
+
+  project                      = var.project
+  name_prefix                  = var.name_prefix
+  github_organization          = var.github_organization
+  github_repository            = var.github_repository
+  github_branch                = var.github_branch
+  role_name                    = var.github_actions_role_name
+  rds_master_password_ssm_name = var.rds_master_password_ssm_name
+  ssm_kms_key_id               = var.ssm_kms_key_id
+  github_oidc_thumbprints      = var.github_oidc_thumbprints
+}
+
